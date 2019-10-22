48f31bec3c98659b07b46d634923824ece42ec01
Maxim Gontarev <maxking20123@gmail.com>
2019-10-22 11:44:32 +0300

    inittial commit
    https://www.hackerrank.com/rest/contests/master/challenges/apple-and-orange/hackers/maxking20123/download_solution

---
+++ main.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int count = 0;
+        for (var i = 0; i < apples.Length; i++) {
+            var d = apples[i] + a;
+
+            if (d >= s && d <= t) {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count.ToString());
+
+        count = 0;
+        for (var i = 0; i < oranges.Length; i++) {
+            var d = oranges[i] + b;
+
+            if (d >= s && d <= t) {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count.ToString());
+    }
+
+    static void Main(string[] args) {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+        ;
+
+        int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
